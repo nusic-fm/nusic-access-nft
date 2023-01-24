c4a38a9ad1b9b93405e2b172426f232d3d7bd243
zeeshanhanif <zeeshanhanif@gmail.com>
2023-01-24 23:47:12 +0500

    Added scripts for pause and unpause contracts

---
+++ scripts/04_pause.ts
@@ -0,0 +1,30 @@
+import { BigNumber } from 'ethers';
+import { ethers } from 'hardhat';
+import { NusicAccessNFT, NusicAccessNFT__factory } from '../typechain';
+const addresses = require("./address.json");
+/*
+* Main deployment script to deploy all the relevent contracts
+*/
+async function main() {
+  const [owner, addr1] = await ethers.getSigners();
+
+  const network = addresses.localhost;
+  
+  const NusicAccessNFT:NusicAccessNFT__factory =  await ethers.getContractFactory("NusicAccessNFT");
+  const nusicAccessNFT:NusicAccessNFT = await NusicAccessNFT.attach(network.nusicAccessNFT);
+  console.log("NusicAccessNFT Address:", nusicAccessNFT.address);
+  const txt = await nusicAccessNFT.pause();
+  console.log("nusicAccessNFT.pause() txt.hash = ",txt.hash);
+  const txtReceipt = await txt.wait();
+  //console.log("txtReceipt = ",txtReceipt); 
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
