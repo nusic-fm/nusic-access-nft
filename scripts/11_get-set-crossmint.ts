b29884137995e8d18b3df1991d930bcdaa8d3ca4
zeeshanhanif <zeeshanhanif@gmail.com>
2023-02-01 23:42:20 +0500

    Added script to update crossmint address

---
+++ scripts/11_get-set-crossmint.ts
@@ -0,0 +1,35 @@
+import { BigNumber } from 'ethers';
+import { ethers } from 'hardhat';
+import { NusicAccessNFT, NusicAccessNFT__factory } from '../typechain';
+const addresses = require("./address.json");
+/*
+* Main deployment script to deploy all the relevent contracts
+*/
+async function main() {
+  const [owner, addr1] = await ethers.getSigners();
+
+  const network = addresses.localhost;
+  
+  const NusicAccessNFT:NusicAccessNFT__factory =  await ethers.getContractFactory("NusicAccessNFT");
+  const nusicAccessNFT:NusicAccessNFT = await NusicAccessNFT.attach(network.nusicAccessNFT);
+  console.log("NusicAccessNFT Address:", nusicAccessNFT.address);
+
+  //const txt = await nusicAccessNFT.setCrossmintAddress("0xDa30ee0788276c093e686780C25f6C9431027234");
+  const txt = await nusicAccessNFT.setCrossmintAddress(owner.address);
+  console.log("nusicAccessNFT.setCrossmintAddress txt.hash = ",txt.hash);
+  const txtReceipt = await txt.wait();
+  //console.log("txtReceipt = ",txtReceipt); 
+
+  const crossmintAddress = await nusicAccessNFT.crossmintAddress();
+  console.log("nusicAccessNFT.crossmintAddress = ",crossmintAddress);
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
